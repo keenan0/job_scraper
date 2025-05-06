f55dbf29901a6c240793c38dca24e2cf5b4cf4a0
Alex-1611 <alex_timplarescu@yahoo.com>
2025-05-06 16:50:42 +0300

    Created search class v2

--- job_model.py
+++ job_model.py
@@ -1,20 +1,11 @@
 class Job:
-    def __init__(self, title, company, location, link, description):
+    def __init__(self, title, company, location, link, description = None):
         self.title = title
         self.company = company
         self.location = location
         self.link = link
         self.description = description
         self.saved = False
-
-    def to_dict(self):
-        return {
-            "Title": self.title,
-            "Company": self.company,
-            "Location": self.location,
-            "Link": self.link,
-            "Saved": self.saved
-        }
 
     def __str__(self):
         return f"{self.saved} {self.title} {self.company} {self.location} {self.link}"
